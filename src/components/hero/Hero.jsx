58be9eeecf3f76c51c95a6f4127f88b453289d09
golamrabbi73 <golamrabbi73hr@gmail.com>
2026-05-11 16:51:08 +0600

    fix hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-        <section className='container py-24 bg-[#F8FAFC] to-white text-center'>
+        <section className='container mx-auto py-24 bg-[#F8FAFC] to-white text-center'>
             <h1 className='text-5xl font-heading font-bold text-[#111827]'>Discover Your Perfect Aesthetic</h1>
             <Link href="/all-tiles">
                 <button className='btn mt-6 px-6 py-3 bg-blue-600 text-white hover:bg-blue-700 rounded-lg transition'>
